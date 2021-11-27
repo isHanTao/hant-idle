8082cd00e406964116d086b49e62ae418e634ca2
hantao <hantao@fanxiapp.com>
2021-11-27 14:19:30 +0800

    init

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <menus/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import menus from "./layout/menus";
+
+export default {
+  name: 'Home',
+  components: {
+    menus
+  }
+}
+</script>
